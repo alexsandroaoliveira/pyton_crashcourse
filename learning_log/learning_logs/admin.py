7859c48dbbd80abe314f481831815b8d6b617f5d
Alexsandro Oliveira <alexsandroaoliveira@gmail.com>
2023-04-15 10:23:07 -0300

    feat: Learning logs

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from learning_logs.models import Topic
+from learning_logs.models import Entry
 
 # Register your models here.
-from models import Topic
-
 admin.site.register(Topic)
+admin.site.register(Entry)
